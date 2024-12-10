b1a01906ecb91bd2e2e83eac7d35b12e14b84fe8
Patrick Bär <patrick.baer00@gmail.com>
2024-12-10 14:48:39 +0100

    final version of reverse

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,11 +1,5 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    let mut output = "".to_string();
-    for i in 0..input.len() {
-        let c = input.chars().nth(i);
-        match c {
-            Some(c) => output = format!("{}{}", c, output),
-            None => continue,
-        }
-    }
-    output
+    input.graphemes(true).rev().collect()
 }
