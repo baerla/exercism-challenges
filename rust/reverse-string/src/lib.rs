155238d39e253d44da1de0b617457000ca727993
Patrick Bär <patrick.baer00@gmail.com>
2024-12-10 14:43:57 +0100

    possiblity 2 of reverse

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    let mut output = "".to_string();
+    for i in 0..input.len() {
+        let c = input.chars().nth(i);
+        match c {
+            Some(c) => output = format!("{}{}", c, output),
+            None => continue,
+        }
+    }
+    output
 }
