8b501b43a09b479a8b86f6fda6271d0dcdd65600
Patrick Bär <patrick.baer00@gmail.com>
2024-12-10 15:05:11 +0100

    final version of sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -15,86 +15,38 @@ struct ListWrapper<'a, T> {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    let _first = ListWrapper {
+    let first_list = ListWrapper {
         list: _first_list,
         len: _first_list.len(),
     };
-    let _second = ListWrapper {
+    let second_list = ListWrapper {
         list: _second_list,
         len: _second_list.len(),
     };
 
-    let mut state = Comparison::Equal;
-    let min = cmp::min(_first.len, _second.len);
-
-    if _first.len == _second.len {
-        // can only be Equal or Unequal
-        for i in 0..min {
-            match state {
-                Comparison::Equal => {
-                    if _first.list[i] != _second.list[i] {
-                        return Comparison::Unequal;
-                    }
-                }
-                _ => (),
-            }
+    if first_list.len == second_list.len {
+        if first_list.list == second_list.list {
+            return Comparison::Equal;
+        }
+    } else if first_list.len < second_list.len {
+        if is_sublist(first_list, second_list) {
+            return Comparison::Sublist;
         }
     } else {
-        // can only be Sub- or Superlist or Unequal
-        let smaller: ListWrapper<T>;
-        let bigger: ListWrapper<T>;
-        if _first.len > _second.len {
-            state = Comparison::Superlist;
-            smaller = _second;
-            bigger = _first;
-        } else {
-            state = Comparison::Sublist;
-            smaller = _first;
-            bigger = _second;
-        }
-        let mut already_valid = false;
-        let mut old_state = state;
-
-        for i in 0..min {
-            if !already_valid {
-                if smaller.list[i] == bigger.list[i] {
-                    already_valid = true;
-                }
-            } else {
-                if smaller.list[i] != bigger.list[i] {
-                    state = Comparison::Unequal;
-                }
-            }
-        }
-        if state == Comparison::Unequal {
-            state = old_state;
-            for first in 1..bigger.len - min + 1 {
-                already_valid = false;
-                let mut j = first;
-                let mut i = 0;
-                while j < bigger.len && i < min {
-                    if !already_valid {
-                        if smaller.list[i] == bigger.list[j] {
-                            already_valid = true;
-                            j += 1;
-                        }
-                    } else {
-                        if smaller.list[i] != bigger.list[j] {
-                            old_state = state;
-                            state = Comparison::Unequal;
-                            already_valid = false;
-                        }
-                        j += 1;
-                    }
-                    i += 1;
-                }
-                if state == Comparison::Unequal && first != bigger.len - min {
-                    state = old_state;
-                } else {
-                    break;
-                }
-            }
+        if is_sublist(second_list, first_list) {
+            return Comparison::Superlist;
         }
     }
-    return state;
+
+    Comparison::Unequal
+}
+
+fn is_sublist<T: PartialEq>(small: ListWrapper<T>, big: ListWrapper<T>) -> bool {
+    for i in 0..big.len - small.len + 1 {
+        if big.list[i..i + small.len] == *small.list {
+            return true;
+        }
+    }
+
+    false
 }
